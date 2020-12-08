ca498a07313c552de311acd3439ab342d1277e6b
raulstvz <estevezterrados@gmail.com>
2020-12-08 16:16:09 +0100

    'Test commit'

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import { useState, useEffect} from 'react';
-import './App.css';
-import UserCard from "./components/userCard/UserCard"
-import Header from "./components/header/Header"
-import BoardList from './components/boardList/BoardList';
-import PinList from './components/pinList/PinList';
+import { useState, useEffect } from "react";
+import "./App.css";
+import UserCard from "./components/userCard/UserCard";
+import Header from "./components/header/Header";
+import BoardList from "./components/boardList/BoardList";
+import PinList from "./components/pinList/PinList";
 
 function App() {
-
   const [user, setUser] = useState({});
 
-  useEffect(() => {  
+  useEffect(() => {
     fetch("http://localhost:5000/api/users/3")
       .then((response) => response.json())
-      .then((json) => setUser(json))
+      .then((json) => setUser(json));
   }, []);
 
   console.log(user);
 
   return (
     <div className="app__body">
-      <Header 
-        avatar={user.avatar} 
-        fullname={`${user.firstName} ${user.lastName}`}/>
-      <UserCard 
+      <p>Hello world</p>
+      <Header
         avatar={user.avatar}
-        userName={user.userName} 
+        fullname={`${user.firstName} ${user.lastName}`}
+      />
+      <UserCard
+        avatar={user.avatar}
+        userName={user.userName}
         fullName={`${user.firstName} ${user.lastName}`}
         followingCount={user.id}
       />
